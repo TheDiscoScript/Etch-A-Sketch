a9ddcf0392c1825feef479a3063e459d3be7af08
Willdooo <72411792+Willdooo@users.noreply.github.com>
2020-11-25 23:05:12 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ This is project in Odin Project curiculum.
 It's project in which user creates grid made from squares. The user can draw in this grid.
 
 What have I learned in this project:
+    - function changeColorVar(e){
+    switch(e.target.dataset.color){...}}
+    + buttonColor.forEach(button => button.addEventListener('click', changeColorVar))
+    --> event + event.target - this is valuable, it helped me a lot.
+    
+    - while loops can be usefull aswell, dont try to solve everything with forloop
 -foreach
 -x.style.xxxx - css through JS
 -css: display -grid; height,width
--- scipt.js
+++ scipt.js
@@ -1,8 +1,11 @@
 //variables
 const buttonClear = document.querySelector('.clearButton')
+const buttonNew = document.querySelector('.newGame')
 const gridContainer = document.querySelector('.gridContainer')
 const buttonColor = document.querySelectorAll('.colorChoice')
+let color = 'black'
 
+createGrid(20);
 //function to create grid - input; square(input*input) - for loop iteration; 
 //add css style to gridContainer
 //forloop - create and insert divs
@@ -16,6 +19,75 @@ function createGrid(gridNumber){
     }
     //get all divs, iterate through each
     let wholeGrid = gridContainer.querySelectorAll('div')
-    wholeGrid.forEach(oneGrid => oneGrid.style.backgroundColor = 'red')
+    wholeGrid.forEach(oneGrid => oneGrid.addEventListener('mouseover', colorGrid))
 }
-createGrid(4);
+
+//function for drawing colors called in create grid
+// var color in switch expression - at row 5 - created var color
+//color will serve as case switcher with next function changeVarColor
+//random case - calling value from getRandomColor fucntion
+function colorGrid(){
+    switch(color){
+        case 'random':
+            this.style.backgroundColor = getRandomColor();
+            break
+        case 'gradual':
+            this.style.backgroundColor = 'grey' //not really gradual, it's optional in odinProject
+            break
+        case 'black':
+            this.style.backgroundColor = 'black'
+            break
+        default:
+            this.style.backgroundColor = 'black'
+            break;
+    }
+}
+//this function changes variable color
+// connected to the buttons with addeventListener + dataset in html
+//parametr in function is event (clicking button)
+//expression in switch is button's dataset-color which is same as case (might be confusing)
+//it changes var color, which is used in function colorGrid, which is called in createGrid
+function changeColorVar(e){
+    switch(e.target.dataset.color){
+        case 'random':
+            color = 'random'
+            break
+        case 'gradual':
+            color = 'gradual'
+            break
+        default:
+            color = 'black'
+            break
+    }
+}
+//copied from stack overflow!
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+//simple erase button
+function erase(){
+    let wholeGrid = gridContainer.querySelectorAll('div')
+    wholeGrid.forEach(oneGrid => oneGrid.style.backgroundColor = '#fff')
+};
+//funciton for new Game
+//parseint for getting number out of string
+//call createGrid with prompt as argument
+function makeNew(){
+    erase()
+    let gridNumber = parseInt(prompt('Gimme a grid size:'))
+    while(isNaN(gridNumber) || gridNumber < 0 || gridNumber > 100){
+        gridNumber = parseInt(prompt('Pick a number between 1 and 100'))
+    }
+    createGrid(gridNumber)
+}
+
+//eventlisteners
+buttonClear.addEventListener('click', erase)
+buttonNew.addEventListener('click', makeNew)
+buttonColor.forEach(button => button.addEventListener('click', changeColorVar))
+
